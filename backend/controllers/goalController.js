aa2df0f0d06bd4cfd0e150fbfb87b1753d9259b6
thanh khiet <cs.thanhkhiet@gmail.com>
2022-04-13 21:53:21 +0700

    connect to DB

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,35 @@
+const asyncHandler = require('express-async-handler')
+
 // @description Get goals
 // @route       Get api/goals
 // @access      Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @description Set goals
 // @route       Post api/goals
 // @access      Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
     if (!req.body.text){
-        res.status(400).json(message: "Please add a text field")
+        res.status(400)
+        throw new Error("Please add a text field")
     }
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @description Update goals
 // @route       Put api/goals/:id
 // @access      Private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
 // @description Delete goals
 // @route       Delete api/goals/:id
 // @access      Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
 module.exports = { getGoals, setGoals, updateGoals, deleteGoals }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const port = process.env.PORT || 5000
+
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
@@ -9,6 +16,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server started onport ${[port]}`)
